c5217ffc922b8f81cb12d247701dfd8ff29e1eea
aysenurozbay <aysenurozbay1004@gmail.com>
2022-12-25 22:35:01 +0300

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Ayşenur ÖZBAY on 25.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
