e1fdce1c5a54d6fa7383f16d168d54a0dbb48845
aysenurozbay <aysenurozbay1004@gmail.com>
2022-12-26 19:53:59 +0300

    first commit

--- weatherApp/ContentView.swift
+++ weatherApp/ContentView.swift
@@ -9,13 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            //        Color(.blue)
+            //            .edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue,.white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomLeading
+            )
+            .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 8){
+                Text("Tokyo, Japan")
+                    .font(.system(size: 32, weight: .medium ,design: .default))
+                    .foregroundColor(.white)
+//                    .padding(.bottom, 200) // bu şekilde sadece bottomda padding uygular
+                    .padding()
+                VStack {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180 , height: 180)
+                    Text("76 ° ") // derece sembolü için option + shit + 8
+                        .font(.system(size: 70 , weight: .medium))
+                        .foregroundColor(.white)
+                }
+                
+                HStack{
+                    VStack{
+                        Text("TUE")
+                            .foregroundColor(.white)
+                            .font(.system(size: 16 , weight: .bold))
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                        Text( "74°")
+                            .font(.system(size: 28, weight: .medium))
+                            .foregroundColor(.white)
+                        
+                    }
+                }
+                Spacer() // yazının altında kalan kısmı boşluk olarak atar
+
+                
+            }
+            
         }
-        .padding()
     }
 }
 
